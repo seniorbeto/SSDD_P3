f3c50d01bf6298cf1af0717856e54675b25fb050
seniorbeto <100471939@alumnos.uc3m.es>
2025-04-15 20:33:21 +0200

    terminada la 3a práctica ya por fin hostia como odio los rpc

--- src/servidor-rpc.c
+++ src/servidor-rpc.c
@@ -54,21 +54,41 @@ response_t *set_value_1_svc(request_t *req, struct svc_req *rqstp) {
   struct Coord value3;
   value3.x = req->value3.x;
   value3.y = req->value3.y;
-  resp.result =
-          set_value(req->key, req->value1.value1_val, req->N_value2, req->V_value2.V_value2_val, value3);
+  resp.result = set_value(req->key, req->value1.value1_val, req->N_value2, req->V_value2.V_value2_val, value3);
   return &resp;
 }
 
 /* Implementación de la operación GET_VALUE */
 response_t *get_value_1_svc(request_t *req, struct svc_req *rqstp) {
   static response_t resp;
+  /* Reservamos buffers estáticos para la cadena y el arreglo de doubles.
+     Se asume un máximo de 256 caracteres para value1 y hasta 32 doubles en V_value2 */
+  static char value1_buffer[256];
+  static double V_value2_buffer[32];
+  struct Coord value3;
+
   memset(&resp, 0, sizeof(resp));
 
+  /* Asignamos los buffers a los campos de la respuesta */
+  resp.value1.value1_val = value1_buffer;
+  resp.V_value2.V_value2_val = V_value2_buffer;
+
   printf("[INFO] get_value_1_svc: clave=%d\n", req->key);
-  resp.result = get_value(req->key, resp.value1.value1_val, &resp.N_value2, resp.V_value2.V_value2_val, &resp.value3);
+
+  /* Llamamos a la función de negocio; se espera que ésta llene los buffers pasados */
+  resp.result = get_value(req->key, value1_buffer, &resp.N_value2, V_value2_buffer, &value3);
+
+  /* Asignamos la longitud de la cadena obtenida (suponiendo que get_value retorna una cadena terminada en '\0') */
+  resp.value1.value1_len = strlen(value1_buffer);
+  /* Establecemos el tamaño del arreglo de doubles de salida igual al número devuelto en N_value2 */
+  resp.V_value2.V_value2_len = resp.N_value2;
+
+  /* Se asigna la estructura Coord obtenida a la respuesta */
+  resp.value3.x = value3.x;
+  resp.value3.y = value3.y;
+
   return &resp;
 }
-
 /* Implementación de la operación MODIFY_VALUE */
 response_t *modify_value_1_svc(request_t *req, struct svc_req *rqstp) {
   static response_t resp;
@@ -96,6 +116,7 @@ response_t *delete_key_1_svc(request_t *req, struct svc_req *rqstp) {
 response_t *exist_1_svc(request_t *req, struct svc_req *rqstp) {
   static response_t resp;
   memset(&resp, 0, sizeof(resp));
+
 
   printf("[INFO] exist_1_svc: clave=%d\n", req->key);
   resp.result = exist(req->key);
